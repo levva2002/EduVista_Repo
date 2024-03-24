060bcde54ca20a1503590d3c69bdbe1af0cf34e9
levva2002 <158252864+levva2002@users.noreply.github.com>
2024-03-25 04:32:13 +0530

    Help-desk(Front-End)

---
+++ help-desk/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import HelpDesk from './HelpDesk';
+
+function App() {
+  return (
+    <div className="App">
+      <HelpDesk/>
+    
+    </div>
+  );
+}
+
+export default App;
