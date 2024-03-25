14d97aa61f5d536e6f6207b78c65dbceedfdb46f
levva2002 <158252864+levva2002@users.noreply.github.com>
2024-03-25 05:53:46 +0530

    Create App.jsx

---
+++ coursecreate/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CourseCreation from './coursecreation';
+
+function App() {
+  return (
+    <div className="App">
+      <CourseCreation/>
+    </div>
+  );
+}
+
+export default App;
